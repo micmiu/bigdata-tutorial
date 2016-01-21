5ad773558ec6e89cb38190e625ec0d6ca3b391e8
micmiu <sjsky007@gmail.com>
2016-01-21 22:50:53 +0800

    feat:hbase coprocess demo for get enviroment

---
+++ hbase-tutorial/src/main/java/com/micmiu/bigdata/hbase/coprocessor/EnvViewer.java
@@ -0,0 +1,43 @@
+package com.micmiu.bigdata.hbase.coprocessor;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
+import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created coprocessor demo for get enviroment
+ * User: <a href="http://micmiu.com">micmiu</a>
+ */
+public class EnvViewer extends BaseRegionObserver {
+	public static final Log LOG = LogFactory.getLog(EnvViewer.class);
+
+	public static final byte[] FAMILY = Bytes.toBytes("@INFO@");
+
+	public static final byte[] ROW_GETREGION = Bytes.toBytes("@@@GETREGION@@@");
+	public static final byte[] ROW_GETTABLE = Bytes.toBytes("@@@GETTABLE@@@");
+
+	@Override
+	public void preGetOp(ObserverContext<RegionCoprocessorEnvironment> e, Get get, List<Cell> results) throws IOException {
+		LOG.debug("Got preGet for evn key: " + Bytes.toStringBinary(get.getRow()));
+		if (Bytes.equals(get.getRow(), ROW_GETREGION)) {
+			KeyValue kv = new KeyValue(get.getRow(), FAMILY, get.getRow(),
+					Bytes.toBytes(e.getEnvironment().getRegion().getRegionNameAsString()));
+			LOG.debug("coprocess match the row kv: " + kv);
+			results.add(kv);
+		} else if (Bytes.equals(get.getRow(), ROW_GETTABLE)) {
+			KeyValue kv = new KeyValue(get.getRow(), FAMILY, get.getRow(),
+					Bytes.toBytes(e.getEnvironment().getRegion().getTableDesc().getNameAsString()));
+			LOG.debug("coprocess match the row kv: " + kv);
+			results.add(kv);
+		}
+	}
+}
