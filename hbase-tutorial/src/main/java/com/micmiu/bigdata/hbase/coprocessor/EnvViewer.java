3207e4d81e80c13c6a382acd90ed05a606cf02e3
micmiu <sjsky007@gmail.com>
2016-01-23 11:39:19 +0800

    feat:hbase coprocessor get configuration

--- hbase-tutorial/src/main/java/com/micmiu/bigdata/hbase/coprocessor/EnvViewer.java
+++ hbase-tutorial/src/main/java/com/micmiu/bigdata/hbase/coprocessor/EnvViewer.java
@@ -2,6 +2,7 @@ package com.micmiu.bigdata.hbase.coprocessor;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Get;
@@ -22,13 +23,21 @@ public class EnvViewer extends BaseRegionObserver {
 
 	public static final byte[] FAMILY = Bytes.toBytes("@INFO@");
 
+	public static final byte[] ROW_GETCONF = Bytes.toBytes("@@@GETCONF@@@");
 	public static final byte[] ROW_GETREGION = Bytes.toBytes("@@@GETREGION@@@");
 	public static final byte[] ROW_GETTABLE = Bytes.toBytes("@@@GETTABLE@@@");
 
 	@Override
 	public void preGetOp(ObserverContext<RegionCoprocessorEnvironment> e, Get get, List<Cell> results) throws IOException {
 		LOG.debug("Got preGet for evn key: " + Bytes.toStringBinary(get.getRow()));
-		if (Bytes.equals(get.getRow(), ROW_GETREGION)) {
+
+		if (Bytes.equals(get.getRow(), ROW_GETCONF)) {
+			Configuration conf = e.getEnvironment().getConfiguration();
+			KeyValue kv = new KeyValue(get.getRow(), FAMILY, Bytes.toBytes("defaultFS"), Bytes.toBytes(conf.get("fs.defaultFS")));
+			results.add(kv);
+			kv = new KeyValue(get.getRow(), FAMILY, Bytes.toBytes("nameservices"), Bytes.toBytes(conf.get("dfs.nameservices")));
+			results.add(kv);
+		} else if (Bytes.equals(get.getRow(), ROW_GETREGION)) {
 			KeyValue kv = new KeyValue(get.getRow(), FAMILY, get.getRow(),
 					Bytes.toBytes(e.getEnvironment().getRegion().getRegionNameAsString()));
 			LOG.debug("coprocess match the row kv: " + kv);
